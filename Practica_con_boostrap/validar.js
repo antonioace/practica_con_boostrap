47e2addcd8cc1c8df80196f41b1cbbfd70a55787
Antonio <victor@localhost.localdomain>
2020-10-03 01:35:50 -0500

    comit 3

--- Practica_con_boostrap/validar.js
+++ Practica_con_boostrap/validar.js
@@ -1,15 +1,27 @@
-$document.ready(function(){
+var nombre=document.getElementById("nombre");
+var email=document.getElementById("email name");
+var pagina=document.getElementById("sitioweb");
+var mensaje=document.getElementById("mensaje");
 
-$(this.#submit).click(function(){
+var formulario=document.getElementById("contact_form");
+formulario.addEventListener("click",function (evt){
+evt.preventDefault();var mensajesE=[];
+if (nombre=null || nombre=="" ) {
+    mensajesE.push("No deje vacio el campo del nombre");
+}
+if (email=null || email=="") {
+    mensajesE.push("No deje vacio el campo del email");
+}
+if (pagina=null || pagina=="") {
+    mensajesE.push("No deje vacio el campo de la pagina");
+}
+if (mensaje=null || mensaje=="") {
+    mensajesE.push("No deje vacio  el campo ");
+}
 
 
-
-
+error.innerHtml=mensajesE.join("h1");
 
 
 });
 
-
-
-
-});
